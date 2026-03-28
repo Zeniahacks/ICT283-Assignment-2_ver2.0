1cd9497401f5a4c6df0c88b0fddcde1de996ac48
Zeniahacks <elsondoesminecraft@gmail.com>
2026-03-28 18:48:04 +0800

    Date.h doxygen comments imported. Moving to Date.cpp.

--- Date.h
+++ Date.h
@@ -21,33 +21,52 @@ class Date {
      */
     Date();
 
+    /**
+     * @brief Parameterized constructor.
+     * @param d Day of the month (1-31).
+     * @param m Month of the year (1-12).
+     * @param y Year.
+     */
     Date(int d, int m, int y);
 
+    /** @brief Gets the day. @return int Day (1-31). */
     int GetDay() const;
 
+    /** @brief Gets the month. @return int Month (1-12). */
     int GetMonth() const;
 
+    /** @brief Gets the year. @return int Year. */
     int GetYear() const;
 
 
+    /** @brief Sets the day. @param d Day (1-31). */
     void SetDay(int d);
 
+    /** @brief Sets the month. @param m Month (1-12). */
     void SetMonth(int m);
 
+    /** @brief Sets the year. @param y Year. */
     void SetYear (int y);
 
+    /**
+     * @brief Converts date to string format DD/MM/YYYY.
+     * @return std::string Formatted date.
+     */
     std::string toString() const;
 
+    /** @brief Less-than operator for chronological sorting. */
     bool operator<(const Date& other) const;
 
+    /** @brief Greater-than operator for chronological sorting. */
     bool operator>(const Date& other) const;
 
+    /** @brief Equality operator. */
     bool operator==(const Date& other) const;
 
 private:
-    int day;
-    int month;
-    int year;
+    int day;     ///< Day of the month
+    int month;   ///< Month of the year
+    int year;    ///< Year
 };
 
 
