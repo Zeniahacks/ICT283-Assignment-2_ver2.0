2414cb7672b59f0fdf350f10b738e5d35b5b1c78
Zeniahacks <elsondoesminecraft@gmail.com>
2026-03-28 23:30:21 +0800

    Imported time.h file, will proceed to add the doxygen comments.

--- Time.h
+++ Time.h
@@ -2,5 +2,38 @@
 #define TIME_H_INCLUDED
 
 
+/**
+ * @class Time
+ * @brief Represents a specific time of day.
+ * @author Elson Ang
+ * @date 2026
+ *
+ * @details
+ * Stores time in hours, minutes, and seconds. Validates input
+ * to ensure 24-hour clock rules are followed.
+ */
+class Time {
+public:
+
+    Time();
+
+    Time(int h, int m, int s);
+
+    int getHour() const;
+    int getMinute() const;
+    int getSecond() const;
+
+    int toSeconds() const;
+
+    bool operator==(const Time& other) const;
+    bool operator<(const Time& other) const;
+
+private:
+    int hour;
+    int minute;
+    int second;
+
+    bool isValidTime(int h, int m, int s) const;
+};
 
 #endif // TIME_H_INCLUDED
