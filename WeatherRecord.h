30a624fe2b5a6223cc0ff821f8d09f5bc0f85b8a
Zeniahacks <elsondoesminecraft@gmail.com>
2026-03-29 09:14:16 +0800

    Imported WeatherRecord.h doxygen comments. Will proceed to WeatherRecord.cpp.

--- WeatherRecord.h
+++ WeatherRecord.h
@@ -18,26 +18,44 @@
 class WeatherRecord {
 public:
 
+    /** @brief Default constructor. */
     WeatherRecord();
 
+    /**
+     * @brief Parameterized constructor.
+     * @param d Date of the record.
+     * @param t Time of the record.
+     * @param speed Wind speed in km/h.
+     * @param temp Ambient temperature in degrees C.
+     * @param solarRad Solar radiation in W/m2.
+     */
     WeatherRecord(const Date& d, const Time& t, float speed, float temp, float solarRad);
 
+    // Getters
     Date getDate() const;
     Time getTime() const;
     float getSpeed() const;
     float getTemp() const;
     float getSolarRad() const;
 
+    /**
+     * @brief Compares two WeatherRecords chronologically.
+     * @details Used for sorting records in BSTs or Maps.
+     */
     bool operator<(const WeatherRecord& other) const;
 
+    /**
+     * @brief Checks if two WeatherRecords occurred at the exact same Date and Time.
+     * @details Crucial for duplicate validation.
+     */
     bool operator==(const WeatherRecord& other) const;
 
 private:
-    Date d;
-    Time t;
-    float speed;
-    float temp;
-    float solarRad;
+    Date d;           ///< Date of the record
+    Time t;           ///< Time of the record
+    float speed;      ///< Wind speed in km/h
+    float temp;       ///< Ambient temperature in Celsius
+    float solarRad;   ///< Solar radiation in W/m2
 };
 
 
